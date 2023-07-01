cc89b5e06f3c18adc025bb235fe209628ddc5225
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2023-07-01 12:41:39 -0400

    drivers: pwm: Add aliases for PWM shell.
    Allow setting aliases for the PWM shell to give dts pwm devices
    more meaningful names.
    
    example:
    
    pwm_shell: pwm_shell {
      compatible = "pwm-leds";
      aux0: aux0 {
      pwms = <&flexpwm2_pwm0 0 PWM_HZ(50) PWM_POLARITY_NORMAL>;
      };
    };
    
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- drivers/pwm/pwm_shell.c
+++ drivers/pwm/pwm_shell.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2019 Vestas Wind Systems A/S
- *
+ * Copyright (c) 2023 Rudis Laboratories
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -12,6 +12,24 @@
 #include <zephyr/shell/shell.h>
 #include <zephyr/drivers/pwm.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+struct iter_entry {
+    char* name;
+    struct pwm_dt_spec spec;
+};
+
+#define PWM_SHELL_NODE DT_NODE_EXISTS(DT_NODELABEL(pwm_shell))
+
+#if PWM_SHELL_NODE
+#define PWM_ENTRY(node_id) { .name = DT_NODE_FULL_NAME(node_id), .spec = PWM_DT_SPEC_GET(node_id)},
+#endif
+
+struct iter_entry pwm_table[] = {
+#if PWM_SHELL_NODE
+    DT_FOREACH_CHILD(DT_NODELABEL(pwm_shell), PWM_ENTRY)
+#endif
+};
 
 struct args_index {
 	uint8_t device;
@@ -28,6 +46,17 @@ static const struct args_index args_indx = {
 	.pulse = 4,
 	.flags = 5,
 };
+#if PWM_SHELL_NODE
+struct args_alias_index {
+	uint8_t alias_name;
+	uint8_t pulse;
+};
+
+static const struct args_alias_index args_alias_indx = {
+	.alias_name = 1,
+	.pulse = 2,
+};
+#endif
 
 static int cmd_cycles(const struct shell *sh, size_t argc, char **argv)
 {
@@ -126,6 +155,90 @@ static int cmd_nsec(const struct shell *sh, size_t argc, char **argv)
 	return 0;
 }
 
+
+static int cmd_list_aliases(const struct shell *sh)
+{	
+#if PWM_SHELL_NODE
+	if (sizeof(pwm_table) > 0) {
+		shell_print(sh, "******\033[31mPWM ALIASES\033[0m******");
+		for(int i = 0; i < sizeof(pwm_table)/sizeof(pwm_table[0]) ; i++)
+		{	
+			shell_print(sh,
+				"\033[31m%s\033[0m: device->\033[33m%s\033[0m, "
+				"channel->\033[33m%d\033[0m, period->\033[33m%.3f\033[0mus",
+				pwm_table[i].name, pwm_table[i].spec.dev->name,
+				pwm_table[i].spec.channel, (double)(pwm_table[i].spec.period/1e3));
+		}
+		return 0;
+	}
+#endif
+	shell_error(sh, "pwm_shell node has no PWM aliases listed in the dts.");
+
+	return 0;
+}
+
+static int cmd_usec_alias(const struct shell *sh, size_t argc, char **argv )
+{
+#if PWM_SHELL_NODE
+	if (sizeof(pwm_table) > 0) {
+		char* alias;
+		uint32_t pulse;
+		int err;
+		alias = argv[args_alias_indx.alias_name];
+		pulse = strtoul(argv[args_alias_indx.pulse], NULL, 0);
+		for(int i = 0; i < sizeof(pwm_table)/sizeof(pwm_table[0]) ; i++)
+		{	
+			if (!strcmp(alias, pwm_table[i].name)) {
+				err = pwm_set_pulse_dt(&pwm_table[i].spec, PWM_USEC(pulse));
+				if (err < 0) {
+					shell_error(sh, "Error %d: %s failed to set pulse width %d",
+						err, alias, pulse);
+					return 0;
+				}
+				return 0;
+			}
+			
+		}
+		shell_error(sh, "Error: no alias matched %s, unable to set pulse %d",
+			alias, pulse);
+		return 0;
+	}
+#endif
+	shell_error(sh, "pwm_shell node has no PWM aliases listed in the dts.");
+
+	return 0;
+}
+
+static int cmd_nsec_alias(const struct shell *sh, size_t argc, char **argv )
+{
+#if PWM_SHELL_NODE
+	if (sizeof(pwm_table) > 0) {
+		char* alias;
+		uint32_t pulse;
+		int err;
+		alias = argv[args_alias_indx.alias_name];
+		pulse = strtoul(argv[args_alias_indx.pulse], NULL, 0);
+		for(int i = 0; i < sizeof(pwm_table)/sizeof(pwm_table[0]) ; i++)
+		{	
+			if (!strcmp(alias, pwm_table[i].name)) {
+				err = pwm_set_pulse_dt(&pwm_table[i].spec, pulse);
+				if (err < 0) {
+					shell_error(sh, "Error %d: %s failed to set pulse width %d", err, alias, pulse);
+					return 0;
+				}
+				return 0;
+			}
+			
+		}
+		shell_error(sh, "Error: no alias matched %s, unable to set pulse %d", alias, pulse);
+		return 0;
+	} 
+#endif
+	shell_error(sh, "pwm_shell node has no PWM aliases listed in the dts.");
+
+	return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(pwm_cmds,
 	SHELL_CMD_ARG(cycles, NULL, "<device> <channel> <period in cycles> "
 		      "<pulse width in cycles> [flags]", cmd_cycles, 5, 1),
@@ -133,6 +246,11 @@ SHELL_STATIC_SUBCMD_SET_CREATE(pwm_cmds,
 		      "<pulse width in usec> [flags]", cmd_usec, 5, 1),
 	SHELL_CMD_ARG(nsec, NULL, "<device> <channel> <period in nsec> "
 		      "<pulse width in nsec> [flags]", cmd_nsec, 5, 1),
+	SHELL_CMD_ARG(list_aliases, NULL, "list_aliases", cmd_list_aliases, 1, 0),
+	SHELL_CMD_ARG(usec_alias, NULL, "<device> <pulse width in usec> ", 
+		      cmd_usec_alias, 3, 0),
+	SHELL_CMD_ARG(nsec_alias, NULL, "<device> <pulse width in nsec> ", 
+		      cmd_nsec_alias, 3, 0),
 	SHELL_SUBCMD_SET_END
 );
 
